349fee386c81373bf324ada659dd06dd906238d6
TheKerbalEngineer <henrymasebergtomlinson@gmail.com>
2024-02-19 08:36:32 -0600

    e

--- Src/bootloader/stage2/string.c
+++ Src/bootloader/stage2/string.c
@@ -53,13 +53,8 @@ unsigned strlen(const char* str)
     return len;
 }
 
-char *strcat(char *dest, const char *src)
+char* strcat(char *restrict dst, const char *restrict src)
 {
-    long int i,j;
-    for (i = 0; dest[i] != '\0'; i++)
-        ;
-    for (j = 0; src[j] != '\0'; j++)
-        dest[i+j] = src[j];
-    dest[i+j] = '\0';
-    return dest;
+    strcpy(dst + strlen(dst), src);
+    return dst;
 }
